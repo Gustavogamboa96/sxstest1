e86225010e839df5002e3d0c9e0496d1b4a899d3
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2024-11-02 12:21:50 -0400

    trying .env for api key wo npm

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import VerticalScrollProgressBar from './components/VerticalScrollProgressBar';
 import Signup from './components/Signup';
+
 
 
 
